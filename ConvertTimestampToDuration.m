65690a836e8c415c5c33fab66050f1a7f43d812c
IgnitiousVukosiMboweni <109460388+IgnitiousVukosiMboweni@users.noreply.github.com>
2022-09-22 16:43:56 +0200

    Delete ConvertTimestampToDuration.m

--- ConvertTimestampToDuration.m
+++
@@ -1,7 +0,0 @@
-% This code converts the Timestamp column in the data to a 
-% ..relative duration array by subtracting the first element from 
-% ..all the others
-
-Tt = timetable(datetime(df.Timestamp),randn(length(df.Timestamp),1));
-dt = Tt.Time-Tt.Time(1);
-Duration = timetable(dt,Tt.Var1);
